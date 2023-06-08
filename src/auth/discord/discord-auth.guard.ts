be51fd6b319df2371de0dffe3e6f2d3a8acfe303
11tuvork28 <zofi@xamh.de>
2023-06-08 14:19:12 +0200

    Add error handling

--- src/auth/discord/discord-auth.guard.ts
+++ src/auth/discord/discord-auth.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class DiscordAuthGuard extends AuthGuard('discord') {}
+export class DiscordAuthGuard extends AuthGuard('discord') {
+  handleRequest(err: any, user: any, _: any) {
+    if (err || !user) {
+      throw new BadRequestException();
+    }
+    return user;
+  }
+}
