4272f635dee55b1934cb85853585ff99d750be33
11tuvork28 <zofi@xamh.de>
2023-08-27 20:56:57 +0200

    Add ApiBearerAuth() to AppController for docs

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/jwt/guards/jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @Get()
   getHello(): string {
     return this.appService.getHello();
