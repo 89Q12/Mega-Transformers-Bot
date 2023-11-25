406369d86440d456e3d7f02dd279548674f1492a
11tuvork28 <zofi@xamh.de>
2023-11-25 13:40:14 +0100

    Fix compilation error

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,16 +1,8 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt/guards/jwt-auth.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @UseGuards(JwtAuthGuard)
-  @ApiBearerAuth()
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-}
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
+export class AppController {}
