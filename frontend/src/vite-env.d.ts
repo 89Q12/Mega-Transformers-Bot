5a0cac628ff9d44ff744a6020a989d70b33f0149
Yuri Becker <hi@yuri.li>
2023-12-24 00:16:52 +0100

    Add type definition for frontend env vars

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  /**
+   * Temporary workaround for not having a guild
+   * selection screen.
+   */
+  readonly VITE_GUILD_ID: string;
+  readonly VITE_DISCORD_OAUTH_CLIENT_ID: string;
+  readonly VITE_DISCORD_CALLBACK_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
