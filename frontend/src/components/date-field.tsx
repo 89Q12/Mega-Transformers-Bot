3961531a9e3c25eb328a13a4d35b269c9418a825
Yuri Becker <hi@yuri.li>
2024-01-24 00:38:30 +0100

    Improve date field

--- frontend/src/components/date-field.tsx
+++ frontend/src/components/date-field.tsx
@@ -1,7 +1,56 @@
-import { FC } from 'react';
-import { Input } from '@chakra-ui/input';
+import { Icon } from '@chakra-ui/icon';
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input';
+import { FC, forwardRef } from 'react';
+import DatePicker from 'react-datepicker';
+import { HiCalendarDays } from 'react-icons/hi2';
+import { useDateTimeFormatter } from '../hooks/ui/use-date-time-formatter.tsx';
 import { FieldProps } from './with-form-control.tsx';
+import 'react-datepicker/dist/react-datepicker.css';
+import './date-field.css';
 
-export const DateField: FC<FieldProps<string>> = ({ helpers, ...field }) => (
-  <Input type={'datetime-local'} {...field} />
-);
+export const DateField: FC<FieldProps<Date | string | null>> = ({
+  helpers,
+  ...field
+}) => {
+  const dateTimeFormatter = useDateTimeFormatter({
+    dateStyle: 'short',
+    timeStyle: 'short',
+  });
+  const CustomInput = forwardRef<
+    HTMLInputElement,
+    { value?: Date | string; onClick?: () => void }
+  >(({ value, onClick }, ref) => (
+    <InputGroup>
+      <InputLeftElement>
+        <Icon as={HiCalendarDays} />
+      </InputLeftElement>
+      <Input
+        value={
+          value
+            ? dateTimeFormatter.format(
+                value instanceof Date ? value : new Date(value),
+              )
+            : ''
+        }
+        onClick={onClick}
+        isReadOnly
+        ref={ref}
+      />
+    </InputGroup>
+  ));
+  return (
+    <DatePicker
+      {...field}
+      customInput={<CustomInput />}
+      showTimeSelect
+      showPopperArrow={false}
+      popperPlacement="auto"
+      value={
+        field.value instanceof Date
+          ? field.value.toString()
+          : field.value ?? undefined
+      }
+      onChange={(value) => helpers.setValue(value)}
+    />
+  );
+};
