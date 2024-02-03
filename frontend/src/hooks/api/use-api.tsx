413a1e4d489741761a1ae11fbb2f1fdc6967c9bf
11tuvork28 <zofi@xamh.de>
2024-02-03 11:33:17 +0100

    Fix yarn lock file and api urls

--- frontend/src/hooks/api/use-api.tsx
+++ frontend/src/hooks/api/use-api.tsx
@@ -2,12 +2,10 @@ import { useContext, useMemo } from 'react';
 import wretch from 'wretch';
 import { SelfContext } from '../../state/self.context.tsx';
 import QueryStringAddon from 'wretch/addons/queryString';
-import { normalizeUrlMiddleware } from './normalize-url-middleware.tsx';
 import { useGuildId } from '../state/use-guild-id.tsx';
+import { normalizeUrlMiddleware } from './normalize-url-middleware.tsx';
 
-const instance = wretch(import.meta.env.VITE_API_URL)
-  .middlewares([normalizeUrlMiddleware])
-  .addon(QueryStringAddon);
+const instance = wretch().addon(QueryStringAddon);
 
 const useApi = ({
   prependGuildId,
@@ -30,20 +28,18 @@ const useApi = ({
             'Having a guild selected is required for this request',
           );
         }
-
-        const parsedUrl = new URL(url);
+        url =
+          url.startsWith('/') && prependGuildId ? url.replace('/', '') : url;
         const prependedUrl = prependGuildId
-          ? `${parsedUrl.origin}/guild/${guildId}/${parsedUrl.pathname.replace(
-              /^\//,
-              '',
-            )}${parsedUrl.search}`
-          : url;
+          ? `${import.meta.env.VITE_API_URL}/guild/${guildId}/${url}`
+          : import.meta.env.VITE_API_URL + url;
         const response = await next(prependedUrl, opts);
         if (response.status === 401) {
           user.clear();
         }
         return response;
       },
+      normalizeUrlMiddleware,
     ]);
   }, [user, guildId, prependGuildId]);
 };
